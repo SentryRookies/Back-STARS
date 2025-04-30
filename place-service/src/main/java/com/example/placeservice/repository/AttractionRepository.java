c54beb90382b15db95fa68c7a30c6982de1a420e
YunHwa0905 <yoho030905@gmail.com>
2025-04-30 13:57:17 +0900

    전체 목록 조회

--- place-service/src/main/java/com/example/placeservice/repository/AttractionRepository.java
+++ place-service/src/main/java/com/example/placeservice/repository/AttractionRepository.java
@@ -2,11 +2,18 @@ package com.example.placeservice.repository;
 
 import com.example.placeservice.entity.Attraction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface AttractionRepository extends JpaRepository<Attraction, Long> {
     boolean existsBySeoulAttractionId(String seoul_attraction_id);
     Optional<Attraction> findByAttractionId(Long attraction_id);
+
+    // 지역 ID로 관광지 목록 조회
+    @Query("SELECT a FROM Attraction a WHERE a.area.areaId = :areaId")
+    List<Attraction> findByAreaAreaId(@Param("areaId") Long areaId);
 }
