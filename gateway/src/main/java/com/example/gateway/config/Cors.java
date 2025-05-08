8d3ef01e9296b810ecc87a040ca57fbacc83ebc8
ha-yun <deu05230@naver.com>
2025-05-08 17:25:10 +0900

    Refactor: cors오류-No 'Access-Control-Allow-Origin' header 수정중

--- gateway/src/main/java/com/example/gateway/config/Cors.java
+++ gateway/src/main/java/com/example/gateway/config/Cors.java
@@ -1,30 +1,34 @@
-//package com.example.gateway.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.reactive.CorsWebFilter;
-//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-//
-//@Configuration
-//public class Cors {
-//
-//    @Bean
-//    public CorsWebFilter corsWebFilter() {
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true); // 인증 정보 포함 X
-//        config.addAllowedOrigin("http://localhost:5173"); // 특정 도메인
-//        config.addAllowedOrigin("http://58.127.241.84:5173");
-//        config.addAllowedOrigin("http://192.168.0.186:5173");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config); // 모든 경로에 적용
-//
-//        return new CorsWebFilter(source);
-//    }
-//
-//
-//
-//}
+package com.example.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class Cors {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // 인증 정보 포함 X
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://192.168.0.186:5173",
+                "http://58.127.241.84:5173"
+        ));
+        config.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // 모든 경로에 적용
+
+        return new CorsWebFilter(source);
+    }
+
+
+
+}
