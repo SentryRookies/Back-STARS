c2b239e3c03af7f2a35b2ea99705c5f3e068fa76
suminmmmmmmm <gims07737@gmail.com>
2025-05-13 13:35:09 +0900

    추가 수정

--- place-service/src/main/java/com/example/placeservice/config/SecurityConfig.java
+++ place-service/src/main/java/com/example/placeservice/config/SecurityConfig.java
@@ -11,8 +11,7 @@ public class SecurityConfig {
     // spring-security 임시 block
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/v3/api-docs/**",
